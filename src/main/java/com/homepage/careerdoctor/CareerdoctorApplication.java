089d7604d8c1b8f4e2792e97159704bbcec3abed
ujii <wcm9763@naver.com>
2024-05-19 02:39:47 +0900

    🎉 New Project

---
+++ src/main/java/com/homepage/careerdoctor/CareerdoctorApplication.java
@@ -0,0 +1,13 @@
+package com.homepage.careerdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerdoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerdoctorApplication.class, args);
+	}
+
+}

c3e0858bf7967af2db3d7ea46d58f83e3a562d7d
ujii <wcm9763@naver.com>
2024-05-25 03:01:36 +0900

    :wrench: User 엔티티 수정

--- src/main/java/com/homepage/careerdoctor/domain/User.java
+++ src/main/java/com/homepage/careerdoctor/domain/User.java
@@ -4,6 +4,8 @@ import com.homepage.careerdoctor.util.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Builder
@@ -12,12 +14,48 @@ import lombok.*;
 @Table(name = "USERS")
 public class User extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "member_id")
+    private Long memberId;
 
     @Column(name = "user_id")
     private String userId;
 
     @Column(name = "password")
     private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private SpecCertificate specCertificate;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<SpecReport> specReports;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Like> likes;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Scrap> scraps;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Vote> votes;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Activity> activities;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Career> careers;//
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Language> languages;//
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Certificate> certificates;//
+
+    @Embedded
+    private Degree degree;//
 }
